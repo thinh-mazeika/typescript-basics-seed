1fd762f73d0c015f19c5ed77899c646fc1aecdf6
Thinh Mazeika <thinh.mazeika@gmail.com>
2020-10-04 22:42:37 -0400

    practices primitive types

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log("Hi TypeScript!");
+const pizzasCount: number = 2;
+
+function discountOrder(orders: number): boolean {
+  return orders >= 3;
+}
+
+if (discountOrder(pizzasCount)) {
+  console.log(`You are entitled to get a discount`);
+} else {
+  console.log(`Buy more than 3 pizzas to get a discount`);
+}
