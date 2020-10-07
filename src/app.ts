f9c576dd5c2e281e24d798bee05642095447d58b
Thinh Mazeika <thinh.mazeika@gmail.com>
2020-10-07 11:07:14 -0400

    interface contracts with implements

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,45 @@
-abstract class Sizes {
-  constructor(public sizes: string[]) {
+interface SizesInterface {
+  AvailableSizes: string[];
+}
+
+abstract class Sizes implements SizesInterface {
+  constructor(protected sizes: string[]) {
     this.sizes = sizes;
   }
 
-  getAvailableSize() {
+  get AvailableSizes() {
     return this.sizes;
   }
 
-  setAvailableSize(sizes: string[]) {
+  set AvailableSizes(sizes: string[]) {
     this.sizes = sizes;
   }
 }
 
+interface PizzaInterface extends SizesInterface {
+  readonly name: string;
+  addToppings(topping: string): void;
+  updateSizes(sizes: string[]): void;
+  toppings: string[];
+}
+
 class Pizza extends Sizes {
   public toppings: string[] = [];
-  constructor(readonly name: string, public sizes: string[]) {
+  constructor(readonly name: string, sizes: string[]) {
     super(sizes);
   }
 
   public addToppings(topping: string) {
     this.toppings.push(topping);
   }
+
+  public updateSizes(sizes: string[]) {
+    this.sizes = sizes;
+  }
 }
 
 const pizza = new Pizza("BBQ", ["small, large"]);
 pizza.addToppings("chicken");
-
-console.log(pizza);
+console.log(pizza.AvailableSizes);
+pizza.updateSizes(["xxlarge"]);
+console.log(pizza.AvailableSizes);
